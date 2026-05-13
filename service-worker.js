a2aa13c15a8091ed2da53f3199b643ca9b7cbd9d
João Otávio <jaobrpereira@gmail.com>
2026-05-13 09:01:26 -0300

    full mobile github pages fix

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,7 @@
-const CACHE_NAME = "vetsync-cache-v1";
-const urlsToCache = [
-    "./",
-    "./index.html",
-    "./app.js",
-    "./manifest.json",
-    "./icon-192.png",
-    "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener('install', event => {
+    self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+    event.respondWith(fetch(event.request));
 });
